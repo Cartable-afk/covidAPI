777ecbf630e9bd7a52311bef09d68fdb8bef1515
hehe <Yanis.monnet1@gmail.com>
2021-05-04 11:33:32 +0200

    Version 0.1

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,30 +10,16 @@ namespace ConsoleApplication1
 {
     class Program
     {
+        private const string VERSION = "0.1";
         private const string GLOBAL_DATA_URL = "https://coronavirusapi-france.now.sh/FranceLiveGlobalData";
         private const string DEPARTEMENT_DATA_URL = "https://coronavirusapi-france.now.sh/LiveDataByDepartement?";
         private const string ALL_DEPARTEMENT = "https://coronavirusapi-france.now.sh/AllLiveData";
 
-        private static HttpClient client;
-
-        static async Task<string> GetGlobalDataAsync()
-        {
-            var data = string.Empty;
-            var response = await client.GetAsync(GLOBAL_DATA_URL);
-
-            if (response.IsSuccessStatusCode)
-            {
-                data = await response.Content.ReadAsStringAsync();
-            }
-
-           return data;
-        }
+       
         
         public static void Main(string[] args)
         {
-            client = new HttpClient();
-            
-            Console.WriteLine("API COVID");
+            Console.WriteLine("API COVID Version {0}", VERSION);
 
             while (true)
             {
@@ -42,15 +28,12 @@ namespace ConsoleApplication1
 
                 switch (commande)
                 {
-                    case "global":
-                        var result = GetGlobalDataAsync().GetAwaiter().GetResult();
-                        Console.WriteLine(result);
-                        break;
-                        
                     case "exit":
                         Environment.Exit(0);
                         break;
 
+                    default:
+                        break;
                 }
             }
         }
