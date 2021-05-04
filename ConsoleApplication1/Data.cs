74627311a7e9c706d72a7372b3e4b1650918a732
hehe <Yanis.monnet1@gmail.com>
2021-05-04 11:22:40 +0200

    Ajoutez des fichiers projet.

---
+++ ConsoleApplication1/Data.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApplication1
+{
+    public class Data
+    {
+        public string code { get; set; }
+        public string nom { get; set; }
+        public string date { get; set; }
+        public int hospitalises { get; set; }
+        public int reanimation { get; set; }
+        public int nouvellesHospitalisations { get; set; }
+        public int nouvellesReanimations { get; set; }
+        public int deces { get; set; }
+        public int gueris { get; set; }
+        public string sourceType { get; set; }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        private const string GLOBAL_DATA_URL = "https://coronavirusapi-france.now.sh/FranceLiveGlobalData";
+        private const string DEPARTEMENT_DATA_URL = "https://coronavirusapi-france.now.sh/LiveDataByDepartement?";
+        private const string ALL_DEPARTEMENT = "https://coronavirusapi-france.now.sh/AllLiveData";
+
+        private static HttpClient client;
+
+        static async Task<string> GetGlobalDataAsync()
+        {
+            var data = string.Empty;
+            var response = await client.GetAsync(GLOBAL_DATA_URL);
+
+            if (response.IsSuccessStatusCode)
+            {
+                data = await response.Content.ReadAsStringAsync();
+            }
+
+           return data;
+        }
+        
+        public static void Main(string[] args)
+        {
+            client = new HttpClient();
+            
+            Console.WriteLine("API COVID");
+
+            while (true)
+            {
+                args = Console.ReadLine().Split(' ');
+                var commande = args[0];
+
+                switch (commande)
+                {
+                    case "global":
+                        var result = GetGlobalDataAsync().GetAwaiter().GetResult();
+                        Console.WriteLine(result);
+                        break;
+                        
+                    case "exit":
+                        Environment.Exit(0);
+                        break;
+
+                }
+            }
+        }
+    }
+}
