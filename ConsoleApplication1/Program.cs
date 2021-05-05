20ee71f48cce5ee4d67eaa26e70aa03b776a59ba
hehe <Yanis.monnet1@gmail.com>
2021-05-05 13:19:16 +0200

    Version finale, ajout de "menu" pour affichr le menu de commande (1.0)

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        private const string VERSION = "0.4";
+        private const string VERSION = "1.0";
         private const string GLOBAL_DATA_URL = "https://coronavirusapi-france.vercel.app/FranceLiveGlobalData";
         private const string DEPARTEMENT_DATA_URL = "https://coronavirusapi-france.vercel.app/LiveDataByDepartement?";
         private const string ALL_DEPARTEMENT = "https://coronavirusapi-france.vercel.app/AllLiveData";
@@ -30,11 +30,35 @@ namespace ConsoleApplication1
             return data;
         }
 
+        static void menu()
+        {
+            var pr = new Printer()
+            {
+                TableWidth = 96
+            };
+
+            pr.Line();
+            pr.Row("Commandes", "Description");
+            pr.Line();
+            pr.Row("global", "Toutes les informations en France");
+            pr.Line();
+            pr.Row("Departement", "Toutes les informations pour un département");
+            pr.Line();
+            pr.Row("allData", "Toutes les informations en France par département");
+            pr.Line();
+            pr.Row("menu", "Affiche le menu des commandes");
+            pr.Line();
+            pr.Row("exit", "Ferme l'application");
+            pr.Line();
+
+        }
+
         public static void Main(string[] args)
         {
             client = new HttpClient();
 
             Console.WriteLine("API COVID Version {0}", VERSION);
+            menu();
 
             while (true)
             {
@@ -52,7 +76,7 @@ namespace ConsoleApplication1
                         var json = GetGlobalDataAsync(GLOBAL_DATA_URL).GetAwaiter().GetResult();
                         var data = JObject.Parse(json).SelectToken("FranceGlobalLiveData").ToObject<List<Data>>().First();
                         printer.Line();
-                        printer.Row("Date", "Pays", "Décès", "Guéris", "Hospitalisé", "Réanimation");
+                        printer.Row("Date", "Département", "Décès", "Guéris", "Hospitalisé", "Réanimation");
                         printer.Line();
                         printer.Row(data.date.ToString("dd/MM/yyyyy"), data.nom, data.deces.ToString(), data.gueris.ToString(), data.hospitalises.ToString(), data.reanimation.ToString());
                         printer.Line();
@@ -64,28 +88,29 @@ namespace ConsoleApplication1
 
                         var departement = DEPARTEMENT_DATA_URL + "Departement=" + Console.ReadLine();
 
-                        try {
+                        try 
+                        {
                             departement.ToString();
-
-                        } catch (Exception e) {
+                        } 
+                        catch (Exception e) 
+                        {
                             Console.WriteLine(e);
                             Console.WriteLine("Entrez un nom de département valide (uniquement du texte)");
-
                         }
 
                         json = GetGlobalDataAsync(departement).GetAwaiter().GetResult();
                         data = JObject.Parse(json).SelectToken("LiveDataByDepartement").ToObject<List<Data>>().First();
 
                         printer.Line();
-                        printer.Row("Date", "Pays", "Décès", "Guéris", "Hospitalisé", "Réanimation");
+                        printer.Row("Date", "Département", "Décès", "Guéris", "Hospitalisé", "Réanimation");
                         printer.Line();
                         printer.Row(data.date.ToString("dd/MM/yyyy"), data.nom, data.deces.ToString(), data.gueris.ToString(), data.hospitalises.ToString(), data.reanimation.ToString());
                         printer.Line();
                         break;
 
                     case "allData":
-                        json = GetGlobalDataAsync(ALL_DEPARTEMENT).GetAwaiter().GetResult();                                //Obtention du JSON en brute
-                        var datas = JObject.Parse(json).SelectToken("allLiveFranceData").ToObject<List<Data>>().ToArray();        //Deserialisation du JSON pour obtenir un Araway 
+                        json = GetGlobalDataAsync(ALL_DEPARTEMENT).GetAwaiter().GetResult();                                        //Obtention du JSON en brute
+                        var datas = JObject.Parse(json).SelectToken("allLiveFranceData").ToObject<List<Data>>().ToArray();          //Deserialisation du JSON pour obtenir un Araway 
                         
                         printer.Line();
                         printer.Row("Date", "Pays", "Décès", "Guéris", "Hospitalisé", "Réanimation");
@@ -100,6 +125,10 @@ namespace ConsoleApplication1
 
                     case "exit":
                         Environment.Exit(0);
+                        break;
+
+                    case "menu":
+                        menu();
                         break;
 
                     default:
